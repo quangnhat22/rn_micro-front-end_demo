b266cca4ed648b2f51494bb9a6d78eb3b3388c81
quangnhat22 <wuangsotech@gmail.com>
2024-02-20 13:13:50 +0700

    fix bug booking app

--- packages/BookingApp/src/screens/HomeScreen.tsx
+++ packages/BookingApp/src/screens/HomeScreen.tsx
@@ -1,12 +1,35 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, Alert} from 'react-native';
 import React from 'react';
+import {Button} from 'react-native-paper';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Booking-HomeScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Featured Services</Text>
+        <Button
+          mode="contained-tonal"
+          onPress={() => Alert.alert('Not implemented yet')}>
+          See All
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
